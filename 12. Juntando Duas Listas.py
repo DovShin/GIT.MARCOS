ef6c5ea973a0c0eb57a47772135f3b06e9445189
Jose Neto <josetenorio.netinho@gmail.com>
2025-08-10 11:39:41 -0300

    solução encontrada de Juntando_Duas_Listas

---
+++ 12. Juntando Duas Listas.py
@@ -0,0 +1,6 @@
+def concatenar_listas(lista1, lista2):
+    return lista1 + lista2
+
+natureza = ["rio", "floresta", "leão", "montanha", "oceano", "estrela", "vento", "cachoeira", "lua", "girassol"]
+tecnologia = ["algoritmo", "API", "blockchain", "código", "software", "nuvem", "hardware", "interface", "pixel", "servidor"]
+print(concatenar_listas(natureza, tecnologia))
