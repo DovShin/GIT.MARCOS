734ec030296b5ad2aa5afbc581c6262f9e8c0178
Jose Neto <josetenorio.netinho@gmail.com>
2025-08-10 11:19:01 -0300

    solução concluida de encontrando o maior numero

---
+++ 2. Encontrando o Maior Número.py
@@ -0,0 +1,6 @@
+def maior_numero(lista):
+    if not lista:
+        return None
+    return max(lista)
+numeros = [987654321,2,7654321,56,1234567,1,88888,3,42,999999,5,1000000000,13,101010,7,444,9,2,13,9]
+print(maior_numero(numeros))
