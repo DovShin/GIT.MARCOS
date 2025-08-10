b3932a58dd3d9b92b50e2e407214c41da3fb0abc
Jose Neto <josetenorio.netinho@gmail.com>
2025-08-10 11:28:27 -0300

    solução encontrada de Buscando_um_Número

---
+++ 6. Buscando um Número.py
@@ -0,0 +1,5 @@
+def buscar_numero(valor, lista):
+    return valor in lista
+numeros = [987654321,2,7654321,56,1234567,1,88888,3,42,999999,5,1000000000,13,101010,7,444,9,2,13,9]
+print(buscar_numero(56, numeros))   # True
+print(buscar_numero(999, numeros))  # False
